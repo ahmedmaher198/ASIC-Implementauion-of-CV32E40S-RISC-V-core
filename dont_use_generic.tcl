f1bbed864f2c7a606398334b657965f76ed75806
Ahmed maher <105322043+ahmedmaher198@users.noreply.github.com>
2024-05-21 21:29:05 +0300

    Add files via upload

---
+++ dont_use_generic.tcl
@@ -0,0 +1,212 @@
+############### ignore small sizes #################
+set_dont_use [get_lib_cells */*0P5*] 
+set_dont_use [get_lib_cells */*0P75*] 
+set_dont_use [get_lib_cells */*_1] 
+set_dont_use [get_lib_cells */*_1P5] 
+
+#set_dont_use [get_lib_cells */*_2] 
+
+set_dont_use [get_lib_cells */*ND2_2*]
+
+set_dont_use [get_lib_cells */*CAPTTAPP6*] 
+
+
+
+
+set_dont_use [get_lib_cells */*_OA22_4*]
+
+set_dont_use [get_lib_cells */*FDPRBQ_V2_4*]
+
+set_dont_use [get_lib_cells */*ND2_3*]
+
+set_dont_use [get_lib_cells */*AO21_4*]
+
+
+set_dont_use [get_lib_cells */*OAI21_3*]
+
+set_dont_use [get_lib_cells */*OA221_4*]
+
+
+set_dont_use [get_lib_cells */*EO3_4*]
+
+set_dont_use [get_lib_cells */*AO2BB2_4*]
+
+
+set_dont_use [get_lib_cells */*AOI311_4*]
+set_dont_use [get_lib_cells */*_OR2_MM_3*]
+set_dont_use [get_lib_cells */*ND2_MM_3*]
+set_dont_use [get_lib_cells */*BUF_S_3*]
+set_dont_use [get_lib_cells */*AOI31_4*]
+set_dont_use [get_lib_cells */*_OR2_MM_2*]
+#MT
+set_dont_use [get_lib_cells */*_AN2_2*] 
+set_dont_use [get_lib_cells */*_OAI32_2*] 
+set_dont_use [get_lib_cells */*_OA21B_2*] 
+set_dont_use [get_lib_cells */*_OR2B_PMM_2*] 
+set_dont_use [get_lib_cells */*_NR4_2*] 
+set_dont_use [get_lib_cells */*_ND2_CDC_2*] 
+set_dont_use [get_lib_cells */*_AN3_2*] 
+set_dont_use [get_lib_cells */*_OR4_2*] 
+set_dont_use [get_lib_cells */*_EN2_2*]
+set_dont_use [get_lib_cells */*_AO2BB2_2*] 
+set_dont_use [get_lib_cells */*_AO221_2*] 
+set_dont_use [get_lib_cells */*_AOI22_2*] 
+set_dont_use [get_lib_cells */*_AO222_2*] 
+set_dont_use [get_lib_cells */*_OA2BB2_V1_2*] 
+set_dont_use [get_lib_cells */*_ADDF_V1_2*] 
+
+
+############# removing all inverters to use the symmetric one only ##############
+set_dont_use [get_lib_cells */*_INV_PECO_*]
+set_dont_use [get_lib_cells */*INV_ECO_*] 
+set_dont_use [get_lib_cells */*_INV_PS_*]
+
+
+set_dont_use [get_lib_cells */*INV_2*] 
+set_dont_use [get_lib_cells */*INV_3*] 
+set_dont_use [get_lib_cells */*INV_4*] 
+set_dont_use [get_lib_cells */*INV_6*] 
+set_dont_use [get_lib_cells */*INV_8*] 
+set_dont_use [get_lib_cells */*INV_10*] 
+set_dont_use [get_lib_cells */*INV_12*] 
+set_dont_use [get_lib_cells */*INV_16*] 
+set_dont_use [get_lib_cells */*_INV_20*]
+
+
+############### no valid via regoin###############
+
+
+set_dont_use [get_lib_cells */*_OAI222_4*]
+
+set_dont_use [get_lib_cells */*_OAI222_4*]
+set_dont_use [get_lib_cells */*_OAI222_4*]
+set_dont_use [get_lib_cells */*_NR2_5*]
+set_dont_use [get_lib_cells */*_AO32_4*]
+set_dont_use [get_lib_cells */*_AO21B_4*]
+set_dont_use [get_lib_cells */*_AOI22_3*]
+set_dont_use [get_lib_cells */*_OA33_4*]
+set_dont_use [get_lib_cells */*_OA33_4*]
+set_dont_use [get_lib_cells */*_OA33_4*]
+set_dont_use [get_lib_cells */*_NR2_3*]
+set_dont_use [get_lib_cells */*_AO2BB2_V1_4*]
+set_dont_use [get_lib_cells */*_AO33_4*]
+set_dont_use [get_lib_cells */*_AO33_4*]
+set_dont_use [get_lib_cells */*_AOI22_4*]
+set_dont_use [get_lib_cells */*_OA21_4*]
+set_dont_use [get_lib_cells */*_ND3_3*]
+set_dont_use [get_lib_cells */*_ND3_3*]
+set_dont_use [get_lib_cells */*_AOI21_3*]
+
+set_dont_use [get_lib_cells */*_OAI311_4*]
+set_dont_use [get_lib_cells */*_AO221_4*]
+set_dont_use [get_lib_cells */*_AO221_4*]
+set_dont_use [get_lib_cells */*_OR3_4*]
+set_dont_use [get_lib_cells */*_MUX2_4*]
+set_dont_use [get_lib_cells */*_OA31_4*]
+set_dont_use [get_lib_cells */*_OA21_2*]
+set_dont_use [get_lib_cells */*_AO2BB2_V1_2*]
+set_dont_use [get_lib_cells */*_OA33_2*]
+set_dont_use [get_lib_cells */*_OA31_2*]
+set_dont_use [get_lib_cells */*_AO21_2*]
+set_dont_use [get_lib_cells */*_AO21B_2*]
+set_dont_use [get_lib_cells */*_NR2_2*]
+set_dont_use [get_lib_cells */*_OAI311_2*]
+set_dont_use [get_lib_cells */*_AO33_2*]
+set_dont_use [get_lib_cells */*_OAI222_2*]
+set_dont_use [get_lib_cells */*_OAI21_2*]
+set_dont_use [get_lib_cells */*_OAI33_2*]
+set_dont_use [get_lib_cells */*_NR3B_2*]
+set_dont_use [get_lib_cells */*_AOI211_2*]
+set_dont_use [get_lib_cells */*_AOI22_6*]
+set_dont_use [get_lib_cells */*_OAI22_2*]
+set_dont_use [get_lib_cells */*_NR2_MM_2*]
+set_dont_use [get_lib_cells */*_AN2B_PMM_2*]
+set_dont_use [get_lib_cells */*_OA221_2*]
+set_dont_use [get_lib_cells */*_AOI21_2*]
+#2
+set_dont_use [get_lib_cells */*OAI33_4*]
+set_dont_use [get_lib_cells */*AO211_2*]
+set_dont_use [get_lib_cells */*_OR2B_PSECO_2*]
+set_dont_use [get_lib_cells */*_MUXI3_2*]
+set_dont_use [get_lib_cells */*_MUXI3_2*]
+set_dont_use [get_lib_cells */*_OAI221_2*]
+set_dont_use [get_lib_cells */*_AOI221_2*]
+set_dont_use [get_lib_cells */*_MUX3_V1M_2*]
+set_dont_use [get_lib_cells */*_MUX3_V1M_2*]
+#3
+set_dont_use [get_lib_cells */*_MUXI3_4*]
+set_dont_use [get_lib_cells */*_ND3B_4*]
+set_dont_use [get_lib_cells */*_AN2_MM_4*]
+set_dont_use [get_lib_cells */*BUF_S_4*]
+set_dont_use [get_lib_cells */*_MUXI3_4*]
+##################### might be blockesd for routing###############
+set_dont_use [get_lib_cells */*_FSDPRBQ_V2_4*]
+set_dont_use [get_lib_cells */*_AO32_4*]
+set_dont_use [get_lib_cells */*_AO221_4*]
+set_dont_use [get_lib_cells */*_AOI21_3*]
+set_dont_use [get_lib_cells */*_AO32_4*]
+set_dont_use [get_lib_cells */*_OAI311_4*]
+set_dont_use [get_lib_cells */*_FDPSBQ_2*]
+set_dont_use [get_lib_cells */*_NR2_MM_*]
+
+set_dont_use [get_lib_cells */*_FDPSBQ_4*]
+
+
+
+############## missing supply pin ##############
+
+set_dont_use [get_lib_cells */*_ISOFSDPQ_PECO_8*]
+set_dont_use [get_lib_cells */*_DCAP_PV1ECO_12*]
+set_dont_use [get_lib_cells */*_ISOFSDPQ_PECO_8*]
+set_dont_use [get_lib_cells */*_DCAP_PV1ECO_12*]
+
+
+############### removing all ECO Cells ################
+set_dont_use [get_lib_cells */*_ECO_*] 
+
+############## remove all large sizes of inv ###########
+set_dont_use [get_lib_cells */*_INV_S_9*]
+
+set_dont_use [get_lib_cells */*_INV_S_10*]
+
+set_dont_use [get_lib_cells */*_INV_S_12*]
+
+#set_dont_use [get_lib_cells */*_INV_S_14*]
+
+set_dont_use [get_lib_cells */*_INV_S_16*]
+
+set_dont_use [get_lib_cells */*_INV_S_20*]
+
+#set_dont_use [get_lib_cells */*_INV_S_18*]
+
+
+# off grid violations from Zroute
+
+set_dont_use [get_lib_cells */*_FDPRBQ_V2*]
+
+
+
+
+
+
+
+###fix rouitng offgrids 
+
+
+
+set_dont_use [get_lib_cells */*_OA22_*]
+set_dont_use [get_lib_cells */*_OR3_*]
+set_dont_use [get_lib_cells */*_MUX2_*]
+set_dont_use [get_lib_cells */*_OA22_*]
+set_dont_use [get_lib_cells */*_NR3_*]
+set_dont_use [get_lib_cells */*_AOI31_*]
+set_dont_use [get_lib_cells */*_AN3_*]
+set_dont_use [get_lib_cells */*_OAI31_*]
+set_dont_use [get_lib_cells */*_NR3_*]
+set_dont_use [get_lib_cells */*FDPRB_V3*]
+set_dont_use [get_lib_cells */*_MUX2_*]
+
+set_dont_use [get_lib_cells */*AO32*]
+
+set_dont_use [get_lib_cells */*_AOINV*]
+
